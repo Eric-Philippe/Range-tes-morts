82b969da490464f24a779fd1b7187433437d3d14
Eric-Philippe <ericphlpp@gmail.com>
2024-11-25 23:38:56 +0100

    Update Database Doc

--- database/init.sql
+++ database/init.sql
@@ -4,19 +4,19 @@ CREATE TABLE Lot(
 );
 
 CREATE TABLE Grave(
-   Id_Grave SMALLINT,
+   ID SMALLINT,
    state BYTE,
    name VARCHAR(50) NOT NULL,
-   PRIMARY KEY(Id_Grave),
+   PRIMARY KEY(ID),
    FOREIGN KEY(name) REFERENCES Lot(name)
 );
 
 CREATE TABLE Dead(
-   Id_Dead COUNTER,
+   ID COUNTER,
    firstname VARCHAR(50),
    lastname VARCHAR(50) NOT NULL,
    entryDate VARCHAR(7),
-   Id_Grave SMALLINT NOT NULL,
-   PRIMARY KEY(Id_Dead),
-   FOREIGN KEY(Id_Grave) REFERENCES Grave(Id_Grave)
+   GraveID SMALLINT NOT NULL,
+   PRIMARY KEY(ID),
+   FOREIGN KEY(GraveID) REFERENCES Grave(ID)
 );
